2d82bcacdbdf25025cb6c216790f3739f63ca5c7
Rodrigo Cabral <rodrigo.cabral@contassconsultoria.com.br>
2020-01-08 14:15:29 -0300

    Template Strings e fail-fast

--- client/js/app/helpers/DataHelper.js
+++ client/js/app/helpers/DataHelper.js
@@ -1,14 +1,13 @@
 class DateHelper {
 
-    statict dataParaTexto(data) {
+    static dataParaTexto(data) {
 
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
       }
 
     static textoParaData(texto) {
-
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+          throw new Error('Deve estar no formato aaaa-mm-dd');
         return new Date(...texto.split('-').map((item,indice) => item - indice % 2));
 
     }
