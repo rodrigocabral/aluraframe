c87bcc42dffaadcf804a9cc354b8220633ba182b
Rodrigo Cabral <rodrigo.cabral@contassconsultoria.com.br>
2020-01-09 09:36:07 -0300

    Totalizando o volume em nosso Template com a função reduce

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -26,6 +26,12 @@ class NegociacoesView {
                 </tr>
             `).join('')}
             </tbody>
+            <tfoot>
+              <td colspan="3"></td>
+              <td>
+                ${model.negociacoes.reduce((total, n) => total += n.volume, 0.0)}
+             </td>
+          </tfoot>
         </table>
             `;
     }
