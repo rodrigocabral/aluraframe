be534fe0e98efe7712e47e6db604ee94c0ec2bba
Rodrigo Cabral <rodrigo.cabral@contassconsultoria.com.br>
2020-01-09 09:19:13 -0300

    Enxugando o código

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -17,16 +17,14 @@ class NegociacoesView {
                 </tr>
             </thead>
             <tbody>
-            ${model.negociacoes.map(n => {
-              return `
+            ${model.negociacoes.map(n => `
                 <tr>
                   <td>${DateHelper.dataParaTexto(n.data)}</td>
                   <td>${n.quantidade}</td>
                   <td>${n.valor}</td>
                   <td>${n.volume}</td>
                 </tr>
-               `
-            }).join('')}
+            `).join('')}
             </tbody>
         </table>
             `;
